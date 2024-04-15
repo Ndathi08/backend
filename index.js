08a7bab0fcfd1692837387112d250f3800b55304
Ndathi08 <hellen.ndathi@strathmore.edu>
2024-04-16 00:09:40 +0300

    feat: add cat controller closes #1

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const catsRouter = require('./routes/cat.routes');
+
+app.use(express.json());
+app.use('/cats', catsRouter);
 
 
-app.get('/', (req, res) => {
-  res.send('root')
-})
-app.get('/test', (req, res) => {
-    res.send('test')
-})
+// app.get('/', (req, res) => {
+//   res.send('root')
+// })
+// app.get('/test', (req, res) => {
+//     res.send('test')
+// })
 
-app.get('/ab?cd', (req, res) => {
-    res.send('ab?cd')
-  })
+// app.get('/ab?cd', (req, res) => {
+//     res.send('ab?cd')
+//   })
+
 app.listen(port, () => {
   console.log(`My server listening on port ${port}`)
 })
--- routes/cat.routes.js
+++ routes/cat.routes.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const catController = require('./controllers/catController');
+const {
+  createCat,
+  readCats,
+  updateCat,
+  deleteCat
+} = require('../controllers/cat.controller');
 
-router.get('/', catController.read);
-router.get('/addCat/:name', catController.create);
+// POST request to create a cat
+router.post('/:id/:name', createCat);
+
+// GET request to read all cats
+router.get('/', readCats);
+
+// PUT request to update a cat by ID
+router.put('/:id/:name', updateCat);
+
+// DELETE request to delete a cat by ID
+router.delete('/:id', deleteCat);
 
 module.exports = router;
