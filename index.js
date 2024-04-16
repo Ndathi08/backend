da823231781c5481265b943f07b53511720dda6a
Ndathi08 <hellen.ndathi@strathmore.edu>
2024-04-16 20:32:44 +0300

    Revert "feat: create a todo backend controller and router closes #3"
    This reverts commit 50dbbeff9688c09dfa9957d5b0354cbecd8e5211.

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const todoRouter = require('./routes/todo.route');
 const catsRouter = require('./routes/cat.routes');
 
 app.use(express.json());
 app.use('/cats', catsRouter);
-app.use('/todos', todoRouter);
 
 
+// app.get('/', (req, res) => {
+//   res.send('root')
+// })
+// app.get('/test', (req, res) => {
+//     res.send('test')
+// })
 
+// app.get('/ab?cd', (req, res) => {
+//     res.send('ab?cd')
+//   })
 
 app.listen(port, () => {
   console.log(`My server listening on port ${port}`)
